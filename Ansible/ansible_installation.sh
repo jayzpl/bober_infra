fced01597f112f9e64feff9ed8e07051b0844417
jayzpl <jzasada06@gmail.com>
2024-10-01 11:51:57 +0000

    feat(ansible): added ansible directory

--- Ansible/ansible_installation.sh
+++ Ansible/ansible_installation.sh
@@ -9,3 +9,9 @@ sudo apt update
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
+ansible --version
+
+sudo mv hosts /etc/ansible/hosts
+
+ansible all -m ping
